f97a210f8a1732ab443107f5686221f2b24866fc
Ahmad <ahmad.waeldemerdash@gmail.com>
2024-08-18 14:46:29 +0300

    stable add search funtionality

---
+++ Wiki/encyclopedia/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from . import util
+
+
+def index(request):
+    if request.method == "POST":
+        user_entry = request.POST.get("q")
+        if util.get_entry(user_entry) == None:
+                entries = util.list_entries()
+                for entry in entries:
+                    if user_entry.lower() in entry.lower():
+                        return render(request, "encyclopedia/search.html", {"entries":entry, "token":1})
+                else:
+                    return render(request, "encyclopedia/search.html", {"entries":util.list_entries(), "token":2})
+        else:
+            url = reverse("entry", args=[user_entry])
+            return HttpResponseRedirect(url)
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry_func(request, title):
+    entry = util.get_entry(title)
+    if entry == None:
+        return render(request, "encyclopedia/error.html")
+    markdowner = Markdown()
+    content = markdowner.convert(entry)
+
+    return render(request, "encyclopedia/entry.html",{"title": title, "content": content})
